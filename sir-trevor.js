e14018c39c975931bacd503fb562587b8b58f06c
e1himself <ioann.voskoboynyk@gmail.com>
2014-03-06 18:07:55 +0200

    Fix reordering DnD listener to be compatible with other DnD functionality. Move generated icons css to separate file.

--- sir-trevor.js
+++ sir-trevor.js
@@ -4,7 +4,7 @@
  * Released under the MIT license
  * www.opensource.org/licenses/MIT
  *
- * 2014-03-04
+ * 2014-03-06
  */
 
 (function ($, _){
@@ -1088,17 +1088,18 @@
         ev.stopPropagation(); // to prevent event handling on outer blocks
   
         var dropped_on = this.$block,
-            item_id = ev.originalEvent.dataTransfer.getData("text/plain"),
-            block = $('#' + item_id);
+            item_id = ev.originalEvent.dataTransfer.getData("text/plain");
   
-        if (!_.isUndefined(item_id) &&
-          !_.isEmpty(block) &&
-          dropped_on.attr('id') != item_id &&
-          dropped_on.attr('data-instance') == block.attr('data-instance')
-        ) {
-          dropped_on.after(block);
+        if (!_.isUndefined(item_id) && item_id.substr(0,9) == 'st-block-') {
+          var block = $('#' + item_id);
+          if (!_.isEmpty(block) &&
+              dropped_on.attr('id') != item_id &&
+              dropped_on.attr('data-instance') == block.attr('data-instance')
+              ) {
+            dropped_on.after(block);
+          }
+          SirTrevor.EventBus.trigger("block:reorder:dropped", item_id);
         }
-        SirTrevor.EventBus.trigger("block:reorder:dropped", item_id);
       },
   
       onDragStart: function(ev) {
